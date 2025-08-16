46f0010b470b4158f2c3aa225393989b67d2bf5f
chenbaojiang <2668328627@qq.com>
2025-08-16 14:39:20 +0800

    测试一下.gitignore是否成功

---
+++ src/main/java/com/yupi/cbjpicturebackend/config/CosClientConfig.java
@@ -0,0 +1,48 @@
+package com.yupi.cbjpicturebackend.config;
+
+
+import com.qcloud.cos.COSClient;
+import com.qcloud.cos.ClientConfig;
+import com.qcloud.cos.auth.BasicCOSCredentials;
+import com.qcloud.cos.auth.COSCredentials;
+
+import com.qcloud.cos.region.Region;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "cos.client")
+@Data
+public class CosClientConfig {
+    /**
+     * 域名
+     */
+    private String host;
+    /**
+     * secretId
+     */
+    private String secretId;
+    /**
+     * 密钥(注意不要泄露)
+     */
+    private String secretKey;
+    /**
+     * 区域
+     */
+    private String region;
+    /**
+     * 域名
+     */
+    private String bucket;
+
+    @Bean
+    public COSClient CosClient() {
+//        初始化用户信息
+        COSCredentials cred = new BasicCOSCredentials(secretId, secretKey);
+//        设置bucket的区域
+        ClientConfig clientConfig = new ClientConfig(new Region(region));
+        return new COSClient(cred,clientConfig);
+    }
+}
